338b33a9d6b9f1f03a57abdaf259edd2636566f2
Stero254 <collinsisaac950@gmail.com>
2026-02-06 13:55:45 +0300

    Update SESSION_ID in settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "Ice~uShqXG", // Put Session Id Here Inside "*"
+  SESSION_ID: "Ice~rtk60R", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "254723431293", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
